a154ca57addbd9b06cd24f98ba178b8a18ea6518
Simon <hawyee@gmail.com>
2025-04-22 17:09:57 +0800

    added divs to categorise the categories and added js variables

---
+++ CarbonCalculator/calculator.js
@@ -0,0 +1,10 @@
+let accomodationBtn = document.getElementById(accomodationBtn)
+let vehicleBtn = document.getElementById(vehicleBtn)
+let restaurantBtn = document.getElementById(restaurantBtn)
+
+// accomodationBtn.addEventListener("click", )
+
+// function switchCategory(category) {
+
+// }
+
